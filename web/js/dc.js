278cb2b825aa458e7b966007c47b39bb28dc1f1d
Gordon Woodhull <gordon@woodhull.com>
2020-05-18 12:26:40 -0400

    artifacts

--- web/js/dc.js
+++ web/js/dc.js
@@ -1,5 +1,5 @@
 /*!
- *  dc 4.0.4
+ *  dc 4.0.3
  *  http://dc-js.github.io/dc.js/
  *  Copyright 2012-2020 Nick Zhu & the dc.js Developers
  *  https://github.com/dc-js/dc.js/blob/master/AUTHORS
@@ -23,7 +23,7 @@
   (global = global || self, factory(global.dc = {}, global.d3, global.d3, global.d3, global.d3, global.d3, global.d3, global.d3, global.d3, global.d3, global.d3, global.d3, global.d3, global.d3, global.d3, global.d3, global.d3, global.d3, global.d3));
 }(this, (function (exports, d3TimeFormat, d3Time, d3Format, d3Selection, d3Dispatch, d3Array, d3Scale, d3Interpolate, d3ScaleChromatic, d3Axis, d3Zoom, d3Brush, d3Timer, d3Shape, d3Collection, d3Geo, d3Ease, d3Hierarchy) { 'use strict';
 
-  const version = "4.0.4";
+  const version = "4.0.3";
 
   class BadArgumentException extends Error { }
 
@@ -2596,6 +2596,8 @@
           return this;
       }
   }
+
+  const baseMixin = () => new BaseMixin();
 
   /**
    * The Color Mixin is an abstract chart functional class providing universal coloring support
@@ -13002,6 +13004,7 @@
   exports.TextFilterWidget = TextFilterWidget;
   exports.afterTransition = afterTransition;
   exports.barChart = barChart;
+  exports.baseMixin = baseMixin;
   exports.boxPlot = boxPlot;
   exports.bubbleChart = bubbleChart;
   exports.bubbleOverlay = bubbleOverlay;
